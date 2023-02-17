63e18a6828a3501fa04b66a25eca1bf52cd7cb35
Dmitriy Orlov <dmitri.orlov.anatolevich@gmail.com>
2023-02-17 16:49:50 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(function () {
+  
+  var mixer = mixitup('.portfolio__content');
+
+  
+
+
+  
+});
+
+const swiper = new Swiper('.swiper', {
+
+  pagination: {
+    el: '.castom-pagination',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  loop: 'true',
+});
